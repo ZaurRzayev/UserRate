d9c291b525a868133620bedd87a371077b949419
Zaur Rzayev <61623422+ZaurRzayev@users.noreply.github.com>
2024-06-27 00:01:06 +0400

    api added

---
+++ app/Http/Middleware/EncryptCookies.php
@@ -0,0 +1,11 @@
+<?php
+
+// app/Http/Middleware/EncryptCookies.php
+namespace App\Http\Middleware;
+
+use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+
+class EncryptCookies extends Middleware
+{
+    //
+}
